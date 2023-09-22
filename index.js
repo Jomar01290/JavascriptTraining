408c5bbcc8995343d68b6f1105d6e4de0051246a
Jomar <dagdagjomar2@gmail.com>
2023-09-23 01:05:31 +0800

    removing input

---
+++ index.js
@@ -0,0 +1,15 @@
+const submitButton = document.querySelector(`#submit`)
+const priceInput = document.querySelectorAll(`.price`)
+const totalAmount = document.querySelector(`#total`)
+
+
+submitButton.addEventListener('click', (e) => {
+    let sum = 0
+    
+    for (let i = 0; i < priceInput.length; i++) {
+        sum += Number(priceInput[i].value)
+    }
+
+    totalAmount.value = sum
+
+})
